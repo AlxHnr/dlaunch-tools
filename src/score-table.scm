64610662adeb88fb9729a1947263c0216a585e3b
Alexander Heinrich <alxhnr@nudelpost.de>
2014-09-15 15:30:39 +0200

    Extended score-table
    Added a function that allows a score table to be written into a file.
    Improved the documentation.

--- src/score-table.scm
+++ src/score-table.scm
@@ -1,27 +1,29 @@
 ;; This unit provides an interface between a score-file and a score-table.
-;; This unit also handles the re-scoring and decay of scores. Such a score
-;; file has a very simple structure: it is a simple scheme file containing
-;; a floating point value followed by an arbitrary amount of pairs. The
-;; floating point value represents the time in seconds of the last scoring.
-;; The pairs associate a string with its score. Before the pairs are
-;; converted into a hash-table, they will be re-scored.
+;; Such a score file has a very simple structure: it is a simple scheme
+;; file containing a floating point value followed by an associative list.
+;; The floating point value represents the time in seconds of the last
+;; scoring. The a-list associates a string with its score. Scores are
+;; floating point values, which decrease continuously. The time needed for
+;; a score to decrease by 1 is specified in seconds in the variable
+;; 'score-decay-interval'. This decrease is applied transparently by the
+;; function 'score-table-read'.
 (declare (unit score-table)
-  (export score-table-read))
+  (uses score-pair)
+  (export score-table-read
+          score-table-write))
 
 (use srfi-69)
 (use posix)
 
 (define score-decay-interval (* 60 60 24 2))
 
-;; This function reads a score-table from a given file and returns a
-;; hash-table which associates the scored string with its new score. It
-;; re-scores the table using the time stamp of the file and throws a file
-;; exception if the file does not exist.
+;; This function reads an existing score file, as described above, and
+;; returns a hash-table which associates a string with its score. It
+;; re-scores the a-list using the timestamp specified in the file and
+;; signals an error if the file does not exist.
 (define (score-table-read filename)
   (define score-file-content (read-file filename))
-  (define score-list (cdr score-file-content))
-  ; The time difference between the current-time and the last time the file
-  ; was scored.
+  (define score-list (cadr score-file-content))
   (define score-decay
     (/ (abs (- (local-time->seconds (seconds->local-time))
                (car score-file-content)))
@@ -34,3 +36,18 @@
         (hash-table-set! table (car score-pair) new-score)))
     score-list)
   table)
+
+;; Writes a score-table to a file. This function is the counterpart to
+;; 'score-table-read'. It signals an error if the file couldn't be created.
+(define (score-table-write score-table filename)
+  (define out (open-output-file filename))
+  (write-line "; Timestamp of last scoring:" out)
+  (write-line
+    (number->string (local-time->seconds (seconds->local-time)))
+    out)
+  (newline out)
+  (write-line "; Associative list of strings and their scores:" out)
+  (pretty-print
+    (sort (hash-table->alist score-table) score-pair>?)
+    out)
+  (close-output-port out))
