02180b2beb1aa0d5cff00e975eec8c56cc9440e5
Alexander Heinrich <alxhnr@nudelpost.de>
2014-09-16 00:17:17 +0200

    Added zlib license header to source files

--- src/score-table.scm
+++ src/score-table.scm
@@ -1,3 +1,24 @@
+; Copyright (c) 2014 Alexander Heinrich <alxhnr@nudelpost.de>
+;
+; This software is provided 'as-is', without any express or implied
+; warranty. In no event will the authors be held liable for any damages
+; arising from the use of this software.
+;
+; Permission is granted to anyone to use this software for any purpose,
+; including commercial applications, and to alter it and redistribute it
+; freely, subject to the following restrictions:
+;
+;    1. The origin of this software must not be misrepresented; you must
+;       not claim that you wrote the original software. If you use this
+;       software in a product, an acknowledgment in the product
+;       documentation would be appreciated but is not required.
+;
+;    2. Altered source versions must be plainly marked as such, and must
+;       not be misrepresented as being the original software.
+;
+;    3. This notice may not be removed or altered from any source
+;       distribution.
+
 ;; This unit provides an interface between a score-file and a score-table.
 ;; Such a score file has a very simple structure: it is a simple scheme
 ;; file containing a floating point value followed by an associative list.
@@ -7,6 +28,7 @@
 ;; a score to decrease by 1 is specified in seconds in the variable
 ;; 'score-decay-interval'. This decrease is applied transparently by the
 ;; function 'score-table-read'.
+
 (declare (unit score-table)
   (uses score-pair)
   (export score-table-read
