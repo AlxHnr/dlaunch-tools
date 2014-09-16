5daefbc552238d6fb9e332865ccbd2638067a336
Alexander Heinrich <alxhnr@nudelpost.de>
2014-09-16 15:41:17 +0200

    Added custom commands for dlaunch-run
    Now it is possible for the user, to define custom commands using the
    config file "dlaunch-run.scm".

--- src/dlaunch-run.scm
+++ src/dlaunch-run.scm
@@ -19,22 +19,52 @@
 ;    3. This notice may not be removed or altered from any source
 ;       distribution.
 
-;; Collects all commands locatable trough the environment variable 'PATH'
-;; and passes them to dmenu.
+;; Collects all commands in 'PATH' and passes them to dmenu. Custom
+;; commands can be added to the config file "dlaunch-run.scm". This config
+;; file can contain lists of strings (commands), which will be listed in
+;; dmenu. The config file can also contain pairs. A pair associates a
+;; custom command with a real command. The custom command will appear in
+;; dmenus listing, while the real command will be executed.
 
 (declare (uses special-paths dmenu-wrapper score-table score-list))
+(use srfi-1 srfi-69 extras posix)
 
-(use srfi-1)
-(use posix)
-
-(define paths (string-split (get-environment-variable "PATH") ":"))
+(define custom-command-file (get-config-file-path "custom-commands.scm"))
 (define score-file-path (get-data-file-path "dlaunch-run.scm"))
 (define score-table
   (if (file-exists? score-file-path)
     (score-table-read score-file-path)
     (make-hash-table)))
 
-; Build a score-list from all commands in 'paths'.
+; Decomposes the content of "dlaunch-run.scm" into a hash table and a score
+; list. The hash table contains the associations and the list contains the
+; strings and its scores.
+(define custom-command-table (make-hash-table))
+(define custom-command-scores
+  (fold
+    (lambda (list-item lst)
+      (if (list? list-item)
+        (fold
+          (lambda (str lst)
+            (score-list-add lst str score-table))
+          lst list-item)
+        (begin
+          (hash-table-set!
+            custom-command-table
+            (car list-item)
+            (cdr list-item))
+          (score-list-add lst (car list-item) score-table))))
+    (list)
+    (if (file-exists? custom-command-file)
+      (condition-case
+        (read-file custom-command-file)
+        ((exn syntax)
+         (error
+           (string-append "broken config file: " custom-command-file))))
+      (list))))
+
+; Build our score list from 'custom-commands-scores' and all commands
+; locatable trough the environment variable 'PATH'.
 (define score-list
   (fold
     (lambda (path lst)
@@ -44,12 +74,14 @@
             (score-list-add lst command score-table))
           lst (directory path))
         ((exn file) lst)))
-    '() paths))
+    custom-command-scores
+    (string-split (get-environment-variable "PATH") ":")))
 
+; Let the user input a string.
 (define selected-command
   (dmenu-select-from-list
     (score-list-harvest score-list)
-  '("-p" "run")))
+    '("-p" "run")))
 
 (if (not selected-command)
   (exit))
@@ -58,4 +90,9 @@
 (ensure-data-dir)
 (score-table-write score-table score-file-path)
 
-(process-execute "/bin/sh" (list "-c" selected-command))
+; Check if the command appears in the custom-command-table and run the
+; command via "/bin/sh".
+(process-execute "/bin/sh" (list "-c" (hash-table-ref/default
+                                        custom-command-table
+                                        selected-command
+                                        selected-command)))
